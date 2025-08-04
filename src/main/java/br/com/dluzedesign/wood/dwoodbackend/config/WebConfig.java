cfef773355b219e70d4e5fdb086bae6c55cc74ac
jvictornascimento <victojoao789@gmail.com>
2025-08-04 19:10:59 -0300

    refactor: cors config

--- src/main/java/br/com/dluzedesign/wood/dwoodbackend/config/WebConfig.java
+++ src/main/java/br/com/dluzedesign/wood/dwoodbackend/config/WebConfig.java
@@ -1,15 +1,27 @@
 package br.com.dluzedesign.wood.dwoodbackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.origin}")
+    private String corOrigin;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET","POST");
+        registry.addMapping("/banner")
+                .allowedOrigins(corOrigin)
+                .allowedMethods("GET");
+        registry.addMapping("/category")
+                .allowedOrigins(corOrigin)
+                .allowedMethods("GET");
+        registry.addMapping("/newsletter")
+                .allowedOrigins(corOrigin)
+                .allowedMethods("POST");
+        registry.addMapping("/products/*")
+                .allowedOrigins(corOrigin)
+                .allowedMethods("GET");
     }
 }
