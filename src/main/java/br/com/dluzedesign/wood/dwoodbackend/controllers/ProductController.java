1e7b960a7539c02c96988d2d9336f54f781e6238
jvictornascimento <victojoao789@gmail.com>
2025-08-25 22:42:20 -0300

    feet: insert and update product in service and controller layer.

--- src/main/java/br/com/dluzedesign/wood/dwoodbackend/controllers/ProductController.java
+++ src/main/java/br/com/dluzedesign/wood/dwoodbackend/controllers/ProductController.java
@@ -1,8 +1,10 @@
 package br.com.dluzedesign.wood.dwoodbackend.controllers;
 
+import br.com.dluzedesign.wood.dwoodbackend.dtos.request.ProductRequestDTO;
 import br.com.dluzedesign.wood.dwoodbackend.dtos.response.ProductCategoryResponseDTO;
 import br.com.dluzedesign.wood.dwoodbackend.dtos.response.ProductResponseDTO;
 import br.com.dluzedesign.wood.dwoodbackend.services.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,5 +31,15 @@ public class ProductController {
     ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id){
         return ResponseEntity.ok(service.getProductById(id));
     }
+    @PostMapping
+    ResponseEntity<ProductResponseDTO> insert(@RequestBody ProductRequestDTO request) {
+        ProductResponseDTO createdProduct = service.insert(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
 
+    @PutMapping(value = "/{id}")
+    ResponseEntity<ProductResponseDTO> update(@PathVariable Long id, @RequestBody ProductRequestDTO request) {
+        ProductResponseDTO updatedProduct = service.update(id, request);
+        return ResponseEntity.ok(updatedProduct);
+    }
 }
